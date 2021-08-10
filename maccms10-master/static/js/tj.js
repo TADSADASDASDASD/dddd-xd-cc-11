0f2e6bf8e010fda7567565ecae13bed1ee60053d
root <xxx@example.com>
2021-08-10 07:38:01 -0700

    update

--- maccms10-master/static/js/tj.js
+++ maccms10-master/static/js/tj.js
@@ -1,1 +1,9 @@
-document.write('统计代码')
+document.write('<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?cb13782c29a1642fdd150145b578a2ff";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>')
