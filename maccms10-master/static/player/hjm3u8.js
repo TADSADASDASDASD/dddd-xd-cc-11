446ba82c3dcebdb95699a15fa2c04d59d314a61f
root <xxx@example.com>
2021-08-11 06:53:01 -0700

    update

---
+++ maccms10-master/static/player/hjm3u8.js
@@ -0,0 +1,2 @@
+MacPlayer.Html = '<iframe src="https://m3u8.hjimg9.com/dp/?url='+MacPlayer.PlayUrl+'" height="'+MacPlayer.Height+'" width="100%" scrolling="no" id="Player" allowfullscreen="true" ></iframe>';
+MacPlayer.Show();
