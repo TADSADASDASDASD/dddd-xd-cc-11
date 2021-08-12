598a665ba80c77bf217147783aa7a66e69ad86ae
root <xxx@example.com>
2021-08-12 06:41:01 -0700

    update

--- maccms10-master/static/player/hjm3u8.js
+++ maccms10-master/static/player/hjm3u8.js
@@ -1,2 +1,2 @@
-MacPlayer.Html = '<iframe src="/ck/index.php?url='+MacPlayer.PlayUrl+'" height="'+MacPlayer.Height+'" width="100%" scrolling="no" id="Player" allowfullscreen="true" ></iframe>';
+MacPlayer.Html = '<iframe src="/ck/?url='+MacPlayer.PlayUrl+'" height="'+MacPlayer.Height+'" width="100%" scrolling="no" id="Player" allowfullscreen="true" ></iframe>';
 MacPlayer.Show();
