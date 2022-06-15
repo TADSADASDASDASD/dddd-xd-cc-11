edc85b141e59d03652789ded7023e84c39ca8d57
root <xxx@example.com>
2022-06-16 02:03:01 +0800

    update

--- maccms10-master/mn.js
+++ maccms10-master/mn.js
@@ -85,7 +85,7 @@ function collectError() {
     
     monitor.errors.push({
       type: 'promise',
-      msg: (e.reason && e.reason.msg) || e.reason || '',
+      msg: (e.reason && e.reason.msg) || e || '',
       time: new Date().getTime(), // 错误发生的时间
     })
   })
